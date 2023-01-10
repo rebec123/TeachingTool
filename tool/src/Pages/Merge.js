c0a9c5a7b11ddec7130a651f94fea7b1e47f3e4b
Rebecca Connell <ed18r22c@leeds.ac.uk>
2023-01-10 11:39:39 +0000

    Added functionality to split arrays

--- tool/src/Pages/Merge.js
+++ tool/src/Pages/Merge.js
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+//import React, { useState } from "react";
+import React from "react";
 import Element from "../Components/Element";
 import { useDrop } from "react-dnd";
+import { toBeVisible } from "../../node_modules/@testing-library/jest-dom/dist/to-be-visible";
 
+const algorithm = ["mergeSort(A)", "If array length > 1", "Split the array in to two halves: S1 and S2",
+    "call mergeSort(S1)", "call mergeSort(S2)", "call merge(S1, S2, A)", "\n", "merge(S1, S2, A)",
+    "i = 0, j = 0, k = 0", "while (i < p and j < q)", "if S1[i] <= S2[j]", "A[k] = S1[i]", "i = i + 1", "else", "A[k] = S2[j]", "j = j + 1",
+    "k = k + 1", "if i = p", "Copy S2[j,…, q - 1] to A", "else", "Copy S1[i,…, p - 1] to A"];
 const elementList = [
     {
         id: 1,
@@ -70,57 +76,50 @@ const elementList = [
 
 ]
 
-function Merge() {
-    const [array, setArray] = useState([]);
-    const [visibleDivs, setVisibleDivs] = useState([1]);
-    const [{ isOver }, drop] = useDrop(() => ({
-        accept: "single-element",
-        drop: (item) => addElementToArr(item.id),
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver(),
-        }),
-    }))
-    //use this for user swapping positions of elements in array
-    const addElementToArr = (id) => {
-        const latestElement = elementList.filter((element) => id == element.id);
-        //setArray((array) => [...array, latestElement[0]]); //adds multiple elements into array
-        setArray([latestElement[0]]); //only one element in array
+class Merge extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: " ",
+            visibleDivs: [1]
+        }; 
     }
 
-    const elementCount = elementList.length;
-
-    function getElementsByDiv(div) {//need some error checking in this baby so don't go out of bounds
+    //Given a div (a position in the graph formed by splitting an array in half recursively), this
+    //function returns the array elements that should be in that sub array
+    getElementsByDiv(div) {
         let parentLen = 0;
         let lowerBound = 0;
         let upperBound = 0;
         let parent = [];
-        if (div == 1) {
+        let elementCount = elementList.length;
+        if (div === 1) {
             return elementList;
         }
-        else if (div == 2) {
+        else if (div === 2) {
             return elementList.filter(element => element.id <= (Math.ceil(elementCount / 2)));
         }
-        else if (div == 3) {
+        else if (div === 3) {
             return elementList.filter(element => element.id > (Math.ceil(elementCount / 2)));
         }
-        else if (div == 4) {
+        else if (div === 4) {
             parentLen = Math.ceil(elementCount / 2);
             upperBound = Math.ceil(parentLen / 2);
             return elementList.filter(element => element.id <= upperBound);
         }
-        else if (div == 5) {
+        else if (div === 5) {
             parentLen = Math.ceil(elementCount / 2);
             lowerBound = Math.ceil(parentLen / 2);
             upperBound = Math.ceil(elementCount / 2);
             return elementList.filter(element => element.id > lowerBound && element.id <= upperBound);
         }
-        else if (div == 6) {
+        else if (div === 6) {
             parentLen = elementCount - Math.ceil(elementCount / 2);
             lowerBound = Math.ceil(elementCount / 2);
             upperBound = lowerBound + Math.ceil(parentLen / 2);
             return elementList.filter(element => element.id > lowerBound && element.id <= upperBound);
         }
-        else if (div == 7) {
+        else if (div === 7) {
             parentLen = elementCount - Math.ceil(elementCount / 2);
             lowerBound = Math.ceil(elementCount / 2) + Math.ceil(parentLen / 2);
             upperBound = lowerBound + (parentLen - Math.ceil(parentLen / 2));
@@ -128,12 +127,12 @@ function Merge() {
         }
         else if (7 < div < 32) {
             let divHalved = div / 2;
-            parent = getElementsByDiv(Math.floor(divHalved));
-            if (divHalved - Math.floor(divHalved) == 0) {
-                return getChildLeft(parent);
+            parent = this.getElementsByDiv(Math.floor(divHalved));
+            if (divHalved - Math.floor(divHalved) === 0) {
+                return this.getChildLeft(parent);
             }
-            else if (divHalved - Math.floor(divHalved) == 0.5) {
-                return getChildRight(parent);
+            else if (divHalved - Math.floor(divHalved) === 0.5) {
+                return this.getChildRight(parent);
             }
             else {
                 console.log("Something went wrong at div = " + div);
@@ -146,14 +145,16 @@ function Merge() {
         }
     }
 
-    function getChildLeft(parent) {
-        if (parent.length == 1) {
+    //Given an array (the parent), this function returns the children that would appear
+    //To the left of the array (the left half of the array's elements)
+    getChildLeft(parent) {
+        if (parent.length === 1) {
             return [];
         }
-        else if (parent.length == 2) {
+        else if (parent.length === 2) {
             return [parent[0]];
         }
-        else if (parent.length == 3 || parent.length==4) {
+        else if (parent.length === 3 || parent.length === 4) {
             return [parent[0], parent[1]];
         }
         else {
@@ -161,58 +162,62 @@ function Merge() {
         }
     }
 
-    function getChildRight(parent) {
-        if (parent.length == 1) {
+    //Given an array (the parent), this function returns the children that would appear
+    //To the right of the array (the right half of the array's elements)
+    getChildRight(parent) {
+        if (parent.length === 1) {
             return [];
         }
-        else if (parent.length == 2) {
+        else if (parent.length === 2) {
             return [parent[1]];
         }
-        else if (parent.length == 3) {
+        else if (parent.length === 3) {
             return [parent[2]];
         }
-        else if (parent.length == 4) {
+        else if (parent.length === 4) {
             return [parent[2], parent[3]];
         }
         else {
             return ['Sub array length unexpected'];
         }
-        deafult:
-        return ['Invalid array length']
     }
 
-    //change to make new layer visible? page should have all divs and all arrays drawn but hide irrelevent sections till its their time to shine
-    const onSplitClick = (div) => {
+    //Given a parent div, this function adds the index of the two children divs to a state array "visibleDivs"
+    //VisibleDivs are the divs that are visible to the user (e.g. after user has split div 1, they can see it's children: div 2 and 3)
+    onSplitClick(div) {
         //children are div*2 and div*2+1
-        console.log("hi " + div);
-        div = 1;
-        setVisibleDivs((visibleDivs) => [...visibleDivs, div * 2, (div * 2) + 1]);
-        console.log(visibleDivs);
-
+        let newElements = [];
+        if (!this.state.visibleDivs.includes(div * 2)) {
+            newElements.push(div * 2);
+        }
+        if (!this.state.visibleDivs.includes((div * 2) + 1)) {
+            newElements.push((div * 2)+1);
+        }
+        this.setState({ visibleDivs: [...this.state.visibleDivs, ...newElements ]});
     }
 
-    const row1 = () => {
+    row1 = () => {
         const result = [];
         for (let i = 2; i < 4; i++) {
-            let elements = getElementsByDiv(i);
-            let visibility = visibleDivs.includes(i) ? "visible" : "hidden";
-            const div = i;
-            console.log(visibility);
-            //visibility prooving difficult!
-            //want to pass div to onclikc and then display that arrays children
-            result.push(
-                <div className="array-and-div" id="array-and-div-">
-                    {console.log("array-and-div-{i}")}
-                <div className="array-split-once">
-                    {
-                        elements?.map((element) => {
-                            return <Element contents={element.contents} id={element.id} />
-                        })}
-                    </div>
-                    <button className="split" onClick={(div) => onSplitClick(div)} >split</button>
+            let elements = this.getElementsByDiv(i);
+            if (this.state.visibleDivs.includes(i)) {
+                result.push(
+                    <div className="array-and-div">
+                        <div className="array-split">
+                            {
+                                elements?.map((element) => {
+                                    return <Element contents={element.contents} id={element.id} />
+                                })
+                            }
+                        </div>
+                        <button className="split" onClick={() => this.onSplitClick(i)} >split</button>
                     </div>
 
-            );
+                );
+            }
+            else {//we still need to fill up the space where the array will go (to keep the layout intact)
+                result.push(<div className="array-and-div"/>);
+            }
         }
         return (
             <div className="sub-stage">
@@ -221,22 +226,26 @@ function Merge() {
         );
     }
 
-    const row2 = () => {
+    row2 = () => {
         const result = [];
         for (let i = 4; i < 8; i++) {
-            let elements = getElementsByDiv(i);
-            let visibility = visibleDivs.includes(i) ? "visible" : "hidden";
-            result.push(
-                <div className="array-and-div" hidden>//temp
-                <div className="array-split-twice">
-                    {
-                        elements?.map((element) => {
-                        return <Element contents={element.contents} id={element.id} />
-                    })}
+            let elements = this.getElementsByDiv(i);
+            if (this.state.visibleDivs.includes(i)) {
+                result.push(
+                    <div className="array-and-div">
+                        <div className="array-split">
+                            {
+                                elements?.map((element) => {
+                                    return <Element contents={element.contents} id={element.id} />
+                                })}
+                        </div>
+                        <button className="split" onClick={() => { this.onSplitClick(i) }} >split</button>
                     </div>
-                    <button className="split" onClick={() => { onSplitClick() }} >split</button>
-                    </div>
-            );
+                );
+            }
+            else {
+                result.push(<div className="array-and-div" />);
+            }
         }
         return (
             <div className="sub-stage">
@@ -245,26 +254,30 @@ function Merge() {
         );
     }
 
-    const row3 = () => {
+    row3 = () => {
         const result = [];
         for (let i = 8; i < 16; i++) {
-            let elements = getElementsByDiv(i);
-            let visibility = visibleDivs.includes(i) ? "visible" : "hidden";
+            let elements = this.getElementsByDiv(i);
             let addSplit = "";
             if (elements.length > 1) {
-                addSplit = <button className="split" onClick={() => { onSplitClick() }} >split</button>;
+                addSplit = <button className="split" onClick={() => { this.onSplitClick(i) }} >split</button>;
             }
-            result.push(
-                <div className="array-and-div" hidden>
-                <div className="array-split-thrice">
-                    {
-                        elements?.map((element) => {
-                            return <Element contents={element.contents} id={element.id} />
-                        })}
-                </div>
-                {addSplit}
-                </div>
-            );
+            if (this.state.visibleDivs.includes(i)) {
+                result.push(
+                    <div className="array-and-div">
+                        <div className="array-split">
+                            {
+                                elements?.map((element) => {
+                                    return <Element contents={element.contents} id={element.id} />
+                                })}
+                        </div>
+                        {addSplit}
+                    </div>
+                );
+            }
+            else {
+                result.push(<div className="array-and-div" />);
+            }
         }
         return (
             <div className="sub-stage">
@@ -273,21 +286,25 @@ function Merge() {
         );
     }
 
-    const row4 = () => {//Note: didn't add split buttons beacuse length should be max 1 so don't need to split furthur
+    row4 = () => {//Note: didn't add split buttons beacuse length should be max 1 so don't need to split furthur
         const result = [];
         for (let i = 16; i < 32; i++) {
-            let elements = getElementsByDiv(i);
-            let visibility = visibleDivs.includes(i) ? "visible" : "hidden";
-            result.push(
-                <div className="array-and-div" hidden>
-                <div className="array-split-thrice">
-                    {
-                        elements?.map((element) => {
-                            return <Element contents={element.contents} id={element.id} />
-                        })}
-                </div>
-                </div >
-            );
+            let elements = this.getElementsByDiv(i);
+            if (this.state.visibleDivs.includes(i)) {
+                result.push(
+                    <div className="array-and-div">
+                        <div className="array-split">
+                            {
+                                elements?.map((element) => {
+                                    return <Element contents={element.contents} id={element.id} />
+                                })}
+                        </div>
+                    </div >
+                );
+            }
+            else {
+                result.push(<div className="array-and-div" />);
+            }
         }
         return (
             <div className="sub-stage">
@@ -295,6 +312,48 @@ function Merge() {
             </div>
         );
     }
+
+    render() {
+        //ToDo: is it centeal???
+        return (
+            <>
+            <h1>Merge Sort</h1>
+                <div className="stage">
+                    <div className="sub-stage">
+                        <div className="array">{ elementList.map((element) => {
+                        return <Element contents={element.contents} id={ element.id }/>
+                        })}
+                    </div>
+                    </div>
+                    <button className="split" onClick={() => { this.onSplitClick(1) }} >split</button>
+                    {this.row1()}
+                    {this.row2()}
+                    {this.row3()}
+                    {this.row4()}
+                    {console.log(this.state.visibleDivs)}
+                </div>
+            </>
+        );
+    }
+}
+
+/*function Merge() {
+    const [array, setArray] = useState([]);
+    const [{ isOver }, drop] = useDrop(() => ({
+        accept: "single-element",
+        drop: (item) => addElementToArr(item.id), we set a sub array as "focused" aqnd thats the array users can rearrange?
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver(),
+        }),
+    }))
+    //use this for user swapping positions of elements in array
+    const addElementToArr = (id) => {
+        const latestElement = elementList.filter((element) => id === element.id);
+        //setArray((array) => [...array, latestElement[0]]); //adds multiple elements into array
+        setArray([latestElement[0]]); //only one element in array
+    }
+
+    
 
     //ToDo: not sure top array is properly central
     return (
@@ -305,12 +364,12 @@ function Merge() {
                 return <Element contents={element.contents} id={ element.id }/>
                 })}
                 </div>
-                <button className="split" onClick={() => { onSplitClick() }} >split</button>
+                <button className="split" onClick={() => { splitClick() }} >split</button>
                 {row1()}
                 {row2()}
                 {row3()}
                 {row4()}
-                <div className="element-target" ref={ drop }>
+                <div className="element-target" ref={ drop }>Add to merge class!!!!!!!!!!!!!!!!
                     {array.map((element) => {
                         return <Element contents={element.contents} id={element.id} />
                     })}
@@ -318,6 +377,6 @@ function Merge() {
             </div>
         </>
     );
-}
+}*/
 
 export default Merge;
